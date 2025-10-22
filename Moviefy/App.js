64167565f80c00f547b19bc7c4ffc3b825d6fa3a
LuksBre <lucasbregadioli8@gmail.com>
2025-10-22 13:56:23 -0300

    Projeto Reiniciado por Erros do Babel

--- Moviefy/App.js
+++ Moviefy/App.js
@@ -1,5 +1,6 @@
-import 'react-native-gesture-handler'
-import AppNavigation from './navigation/appNavigation';
+import 'react-native-gesture-handler';
+import "./global.css";
+import AppNavigation from "./navigation/appNavigation";
 
 export default function App() {
   return (
